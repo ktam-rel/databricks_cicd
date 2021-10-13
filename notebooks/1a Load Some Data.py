d2806c7aa50566a4988acbe83acc5cdbba36c186
ken.tam@relativity.com <ken.tam@relativity.com>
2021-10-13 18:09:24 +0000

    updates to work from lib functions

--- notebooks/1a Load Some Data.py
+++ notebooks/1a Load Some Data.py
@@ -8,40 +8,18 @@
 # MAGIC - https://api.github.com/repos/CSSEGISandData/COVID-19/contents/csse_covid_19_data/csse_covid_19_daily_reports?ref=master
 
 # COMMAND ----------
+
 from relpipeline.data import *
 
 db_utils = get_dbutils(spark)
 mount_str = "/mnt/ktam"
 mount(db_utils, "ktam-test", "analyticsresearch", "analyticsresearch-storageacctscope", "storage-account-key", mount_str)
 
+
 # COMMAND ----------
 
 covid_data_dir_URL = "https://api.github.com/repos/CSSEGISandData/COVID-19/contents/csse_covid_19_data/csse_covid_19_daily_reports?ref=master"
+
 data_json = get_json_from_url(covid_data_dir_URL)
-
-files = get_fileinfo_dataframe_from_json(data_json)
-
-
-from relpipeline.data import get_json_from_url
-
-data_url = "https://api.github.com/repos/CSSEGISandData/COVID-19/contents/csse_covid_19_data/csse_covid_19_daily_reports?ref=master")
-
-files_info = get_json_from_url(data_url)
-
-# function to create dataframe from from json
-
-paths = list(map(lambda f: (f["path"], f["download_url"]), files))
-pathsSchema = ['path', 'download_url']
-
-pathsDF = spark.createDataFrame(paths, pathsSchema)s 
-
-
-# function to download file from info
-def download_covid_data(df):
-    covid_data_file_URL = urllib.request.urlopen(df.download_url)
-    data = covid_data_file_URL.read().decode(covid_data_file_URL.headers.get_content_charset())
-    writepath = '/dbfs/mnt/ktam/' + df.path
-    with open(writepath, 'w') as f:
-        f.write(data)
-
-pathsDF.filter(pathsDF.path.endswith('.csv')).foreach(download_covid_data)
+files = get_fileinfo_dataframe_from_json(spark, data_json)
+download_all(db_utils, "/dbfs" + mount_str + "/data/", files)

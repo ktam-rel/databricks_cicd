c71af74c6023af6003bb655855535803724aae94
Ken Tam <ken.tam@relativity.com>
2021-10-13 13:10:29 -0500

    moves more into lib

--- lib/relpipeline/data.py
+++ lib/relpipeline/data.py
@@ -1,3 +1,6 @@
+import urllib.request  # the lib that handles the url stuff
+import json
+
 def get_dbutils(spark):
         try:
             from pyspark.dbutils import DBUtils
@@ -15,17 +18,26 @@ def mount(dbutils, container, storage_account, scope, key, mount_as):
         mount_point = mount_as,
         extra_configs = {f"fs.azure.account.key.{storage_account}.blob.core.windows.net":dbutils.secrets.get(scope = scope, key = key)})
 
-
-import urllib.request  # the lib that handles the url stuff
-import json
-
 def get_json_from_url(url):
     context = urllib.request.urlopen(url)
     response = context.read()
     encoding = context.info().get_content_charset('utf-8')
     return json.loads(response.decode(encoding))
 
-def get_fileinfo_dataframe_from_json(json_obj):
-    paths = list(map(lambda f: (f["path"], f["download_url"]), json_obj))
-    pathsSchema = ['path', 'download_url']
+def get_fileinfo_dataframe_from_json(spark, json_obj):
+    paths = list(map(lambda f: (f["name"], f["download_url"]), json_obj))
+    pathsSchema = ['name', 'download_url']
     return spark.createDataFrame(paths, pathsSchema)
+
+def download_file_from_dataframe(df, base_path):
+    url = urllib.request.urlopen(df.download_url)
+    data = url.read().decode(url.headers.get_content_charset())
+    writepath = base_path + df.name
+    with open(writepath, 'w') as f:
+        f.write(data)
+
+def download_all(dbutils, dir_path, files):
+    dbutils.fs.rm(dir_path, True)
+    dbutils.fs.mkdirs(dir_path)
+    files.filter(files.name.endswith('.csv')).foreach(lambda f: download_file_from_dataframe(f, dir_path))
+

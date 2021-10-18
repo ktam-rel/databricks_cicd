0d423f8cff5b81c9821575908db7a9b51dce302d
ken.tam@relativity.com <ken.tam@relativity.com>
2021-10-18 22:20:05 +0000

    fixes to refactored notebooks

--- notebooks/2a Transform Some Data.py
+++ notebooks/2a Transform Some Data.py
@@ -2,6 +2,7 @@
 # MAGIC %md ## Process ADLS blobs in container and put data into a DeltaLake table
 
 # COMMAND ----------
+
 from relpipeline.util import *
 
 db_utils = get_dbutils(spark)
@@ -10,13 +11,15 @@ mount(db_utils, "ktam-test", "analyticsresearch", "analyticsresearch-storageacct
 
 
 # COMMAND ----------
+
 from pyspark.sql.functions import col, substring
 from pyspark.sql.types import IntegerType
 
-df = transform_csv_data(spark, mount_str)
+df = transform_csv_data(spark, mount_str + "/data")
 write_df_to_delta_table(df, mount_str)
 
 # COMMAND ----------
+
 spark.read.format("delta").load("/mnt/ktam/delta/output_delta").createOrReplaceTempView("covid_delta")
 spark.sql("SELECT count(*) FROM covid_delta").show()
 spark.sql("SELECT * FROM covid_delta LIMIT 5").show()
